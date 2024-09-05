6436a179d5a2f08c556e6bc6868a6846defb97b5
omprakash umath <omprakash@greenstory.io>
2024-09-05 14:04:09 +0530

    Java interview question

---
+++ solutions/string/find_sum_from_string.java
@@ -0,0 +1,24 @@
+class FindSum {
+    public static void main(String[] args) {
+        String str="03Ju23st 15te5ll10 12me6 3the ti45me 09f567or 06bu7s 23";
+        int num=0;
+        int sum=0;
+        String numStr="";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+                numStr+=str.charAt(i);
+            }else{
+                if(numStr.length()>0)
+                num=Integer.parseInt(numStr);
+                sum=sum+num;
+                numStr="";
+            }
+        }
+        if(numStr.length()>0)
+        {
+            num=Integer.parseInt(numStr);
+            sum=sum+num;
+        }
+        System.out.println(sum);
+    }
+}
